f9fc7a9b8f2792f458eee35925a521fbb82f8837
jrivera6 <jhonny.riverap6@gmail.com>
2019-08-19 21:13:48 -0500

    First commit

---
+++ Caso4/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Caso4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbAños = new System.Windows.Forms.ComboBox();
+            this.cbMes = new System.Windows.Forms.ComboBox();
+            this.dgPedidos = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgPedidos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(183, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Escoger año";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(183, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Escoger mes";
+            // 
+            // cbAños
+            // 
+            this.cbAños.FormattingEnabled = true;
+            this.cbAños.Location = new System.Drawing.Point(277, 48);
+            this.cbAños.Name = "cbAños";
+            this.cbAños.Size = new System.Drawing.Size(121, 21);
+            this.cbAños.TabIndex = 2;
+            this.cbAños.SelectionChangeCommitted += new System.EventHandler(this.cbAños_SelectionChangeCommitted);
+            // 
+            // cbMes
+            // 
+            this.cbMes.FormattingEnabled = true;
+            this.cbMes.Location = new System.Drawing.Point(277, 89);
+            this.cbMes.Name = "cbMes";
+            this.cbMes.Size = new System.Drawing.Size(121, 21);
+            this.cbMes.TabIndex = 3;
+            this.cbMes.SelectionChangeCommitted += new System.EventHandler(this.cbMes_SelectionChangeCommitted);
+            // 
+            // dgPedidos
+            // 
+            this.dgPedidos.AllowUserToAddRows = false;
+            this.dgPedidos.AllowUserToDeleteRows = false;
+            this.dgPedidos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgPedidos.Location = new System.Drawing.Point(50, 136);
+            this.dgPedidos.Name = "dgPedidos";
+            this.dgPedidos.ReadOnly = true;
+            this.dgPedidos.Size = new System.Drawing.Size(595, 265);
+            this.dgPedidos.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgPedidos);
+            this.Controls.Add(this.cbMes);
+            this.Controls.Add(this.cbAños);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgPedidos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbAños;
+        private System.Windows.Forms.ComboBox cbMes;
+        private System.Windows.Forms.DataGridView dgPedidos;
+    }
+}
+
---
+++ Caso4/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Caso4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["db"].ConnectionString);
+
+        public void ListaAños()
+        {
+            using (SqlCommand cmd = new SqlCommand("usp_listaanios", cn))
+            {
+                using (SqlDataAdapter da = new SqlDataAdapter())
+                {
+                    da.SelectCommand = cmd;
+                    da.SelectCommand.CommandType = CommandType.StoredProcedure;
+                    using (DataSet df = new DataSet())
+                    {
+                        da.Fill(df, "pedidos");
+                        cbAños.DataSource = df.Tables["pedidos"];
+                        cbAños.DisplayMember = "anios";
+                        cbAños.ValueMember = "anios";
+                    }
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ListaAños();
+        }
+
+        private void cbAños_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            using (SqlCommand cmd = new SqlCommand("usp_lista_pedidos_mes", cn))
+            {
+                using (SqlDataAdapter da = new SqlDataAdapter())
+                {
+                    da.SelectCommand = cmd;
+                    da.SelectCommand.CommandType = CommandType.StoredProcedure;
+                    da.SelectCommand.Parameters.AddWithValue("@anio", cbAños.SelectedValue);
+
+                    using (DataSet df = new DataSet())
+                    {
+                        da.Fill(df, "pedidos");
+                        cbMes.DataSource = df.Tables["pedidos"];
+                        cbMes.DisplayMember = "meses";
+                        cbMes.ValueMember = "meses";
+                    }
+                }
+            }
+        }
+
+        private void cbMes_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            using (SqlCommand cmd = new SqlCommand("usp_lista_pedidosxmeses", cn))
+            {
+                using (SqlDataAdapter da = new SqlDataAdapter())
+                {
+                    da.SelectCommand = cmd;
+                    da.SelectCommand.CommandType = CommandType.StoredProcedure;
+                    da.SelectCommand.Parameters.AddWithValue("@anio", cbAños.SelectedValue);
+                    da.SelectCommand.Parameters.AddWithValue("@mes", cbMes.SelectedValue);
+
+                    using (DataSet df = new DataSet())
+                    {
+                        da.Fill(df, "Pedidos");
+                        dgPedidos.DataSource = df.Tables["Pedidos"];
+                        //lblPedido.Text = df.Tables["Pedidos"].Rows.Count.ToString();
+                    }
+                }
+            }
+        }
+    }
+}
